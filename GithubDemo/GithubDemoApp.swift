a1e28fa9352c69ca45fa7e422f7dd7b3adeef22d
Oleksii Yakymchuk <oles0305@gmail.com>
2023-08-21 23:09:47 +0300

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Олексій Якимчук on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
