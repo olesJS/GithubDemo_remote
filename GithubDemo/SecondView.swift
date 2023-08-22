e2a46c278efeb3b087b23eaffe50e7d752a2fb70
Oleksii Yakymchuk <oles0305@gmail.com>
2023-08-22 13:27:55 +0300

    added second page

---
+++ GithubDemo/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GithubDemo
+//
+//  Created by Олексій Якимчук on 22.08.2023.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Second page")
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
